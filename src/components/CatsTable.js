8e8428e8eb9e37b5daae95b1f27433c8389085b6
Ezra Paz <jrpaz@bayad.com>
2022-05-10 17:34:31 +0800

    feat: add search bar and change look and feel of the table

--- src/components/CatsTable.js
+++ src/components/CatsTable.js
@@ -10,6 +10,10 @@ import TablePagination from '@mui/material/TablePagination';
 import { getCats } from '../reducers/Cat/CatsAction';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Input } from '@mui/material';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 
 function createData(id, name, origin, weight) {
     return { id, name, origin, weight };
@@ -43,32 +47,45 @@ export default function CatsTable() {
     }, [cats])
 
     return (
-        <TableContainer component={Paper} sx={{ margin: 10 }}>
-            <Table sx={{ minWidth: 650 }} stickyHeader aria-label="sticky  table">
-                <TableHead sx={{ backgroundColor: 'black' }}>
-                    <TableRow>
-                        <TableCell>Id</TableCell>
-                        <TableCell align="left">Name</TableCell>
-                        <TableCell align="left">Origin</TableCell>
-                        <TableCell align="left">Weight&nbsp;(kg)</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows && rows.map((row) => (
-                        <TableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.id}
-                            </TableCell>
-                            <TableCell align="left">{row.name}</TableCell>
-                            <TableCell align="left">{row.origin}</TableCell>
-                            <TableCell align="left">{row.weight}</TableCell>
+        <Paper>
+            <Paper
+                component="form"
+            >
+                <InputBase
+                    placeholder="Search Name"
+                />
+                <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
+            <TableContainer component={Paper} sx={{ maxHeight: 500, minWidth: 1600 }}>
+
+                <Table sx={{ minWidth: 650 }} stickyHeader aria-label="sticky  table">
+                    <TableHead sx={{ backgroundColor: 'black' }}>
+                        <TableRow>
+                            <TableCell>Id</TableCell>
+                            <TableCell align="left">Name</TableCell>
+                            <TableCell align="left">Origin</TableCell>
+                            <TableCell align="left">Weight&nbsp;(kg)</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                    </TableHead>
+                    <TableBody>
+                        {rows && rows.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.id}
+                                </TableCell>
+                                <TableCell align="left">{row.name}</TableCell>
+                                <TableCell align="left">{row.origin}</TableCell>
+                                <TableCell align="left">{row.weight}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
             <TablePagination
                 rowsPerPageOptions={[10, 25, 100]}
                 component="div"
@@ -78,7 +95,6 @@ export default function CatsTable() {
                 onPageChange={handleChangePage}
                 onRowsPerPageChange={handleChangeRowsPerPage}
             />
-        </TableContainer>
-
+        </Paper>
     )
 }
